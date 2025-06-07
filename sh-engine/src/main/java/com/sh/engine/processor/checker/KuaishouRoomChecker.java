12c37ccf4c115186cdb3c1d41fa6905c01920989
walaiwalai <wqc57c@gmail.com>
2025-06-07 17:54:20 +0800

    支持快手直播

--- sh-engine/src/main/java/com/sh/engine/processor/checker/KuaishouRoomChecker.java
+++ sh-engine/src/main/java/com/sh/engine/processor/checker/KuaishouRoomChecker.java
@@ -59,21 +59,19 @@ public class KuaishouRoomChecker extends AbstractRoomChecker {
     }
 
     private Recorder parseStreamData(String htmlStr) {
-        log.warn("kuai shou htmlStr: {}", htmlStr);
         Matcher initialStateMatcher = INITIAL_STATE_PATTERN.matcher(htmlStr);
         if (!initialStateMatcher.find()) {
             return null;
         }
 
         String jsonStr = initialStateMatcher.group(1);
-        log.warn( "kuai shou jsonStr: {}", jsonStr);
         Matcher playListMatcher = PLAY_LIST_PATTERN.matcher(jsonStr);
         if (!playListMatcher.find()) {
             return null;
         }
 
         String playListStr = playListMatcher.group(1) + "}";
-        log.warn( "kuai shou playlistObj: {}", playListStr);
+        log.warn("kuai shou playlistObj: {}", playListStr);
 
         JSONObject playlistObj = JSON.parseObject(playListStr);
         if (playlistObj.containsKey("errorType")) {
