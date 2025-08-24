a8635ffcd931055041562de584fda8b51d36bf6f
walaiwalai <wqc57c@gmail.com>
2025-08-24 17:48:29 +0800

    挂载转mp4报错

--- sh-engine/src/main/java/com/sh/engine/model/ffmpeg/Ts2Mp4ProcessCmd.java
+++ sh-engine/src/main/java/com/sh/engine/model/ffmpeg/Ts2Mp4ProcessCmd.java
@@ -1,6 +1,5 @@
 package com.sh.engine.model.ffmpeg;
 
-import com.sh.engine.annotation.Mounted;
 
 import java.io.File;
 
@@ -9,7 +8,7 @@ import java.io.File;
  * @Date 2025 08 09 00 39
  **/
 public class Ts2Mp4ProcessCmd extends AbstractCmd {
-    public Ts2Mp4ProcessCmd(File tsFile, @Mounted File mp4File) {
+    public Ts2Mp4ProcessCmd(File tsFile, File mp4File) {
         super("");
         this.command = "ffmpeg -i " + tsFile.getAbsolutePath() + " -c copy \"" + mp4File.getAbsolutePath() + "\"";
     }
